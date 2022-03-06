30be2201cea68aae7079d7dc6d545c0297892928
Noureldin7 <noureldin.salah01@eng-st.cu.edu.eg>
2022-03-06 18:05:50 +0200

    initial project version

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+Users=[{"name":"User1","E-mail":"User1@yahoo.com","Password":"Pass1"},{"name":"User2","E-mail":"User2@yahoo.com","Password":"Pass2"}]
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
+
+
+@app.route("/Signup", methods=["POST"])
+def sign_up():
+    email = request.form['Email']
+    username = request.form['Username']
+    password = request.form['Password']
+    User = {"name":username,"E-mail":email,"Password":password}
+    if not user_exists(email=email, username=username, password=password):
+        Users.append(User)
+        return "<h2>New user has been created</h2><a href=http://127.0.0.1:5000/>Return</a>"
+    else:
+        return "<h2>This user already exists</h2>"
+
+def Check(User,username):
+    return (User["name"]==username)
+
+def user_exists(email, username, password):
+    # TODO: check for user if exists, you can use an array as your records.
+    return True in list(map(lambda user: Check(user, username),Users))
+
+app.run(debug=True)    
---
+++ static/js/utils.js
@@ -0,0 +1,6 @@
+function alert_welcome(User="") {
+	Welcome="Welcome"+User
+	alert(Welcome)
+	// TODO: show alert with welcoming message
+	// HINT: check alert()
+}
